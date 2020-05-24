92c7ec1dafc2508d2227bd84643454a71921b751
ragarupa <ragarupadevi.12@gmail.com>
2020-05-24 16:27:22 +0530

    basic commands

---
+++ Java/Corejava/src/main/java/com/datastructures/ArraysIO.java
@@ -0,0 +1,10 @@
+public class ArraysIO {
+    public static void integerArray() {
+        int[] int_arr = new int[5];
+        //int_arr[0] = 1;
+        // 1 2 3  4  5
+        for(int i=0;i<5;i++) {
+            System.out.println(i);
+        }
+    }
+}
